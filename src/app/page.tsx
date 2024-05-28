b02126f4a44494acba5677eb98989c2500cb93d2
57Ajay <57ajay.u@gmail.com>
2024-05-28 11:17:40 +0530

    Just code and chill

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+const Home = ()=>{
+  return(
+    <div className="flex min-h-screen flex-col pt-24 text-center py-2">
+      <h1 className="text-5xl font-bold"> Welcome to the NextWrite</h1>
+      <p>This is a one stop Place for Writing your imaginations and thoughts, Here you can write your stories, poems, articles, blogs and much more. You can also share your work with the world and get feedback from other writers.</p>
+    </div>
+  )
+};
+
+export default Home;
