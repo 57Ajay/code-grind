08c6625b91ac3e0230ea5eb7b03a92ec08055408
57Ajay <57ajay.u@gmail.com>
2024-05-29 19:33:51 +0530

    adding loading components

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -14,7 +14,8 @@ const Header=()=> {
 
   return (
     <header className="flex max-w-[1280px] mx-auto px-4 shadow-lg bg-slate-800 text-zinc-200 rounded-sm justify-between p-4">
-      <div>NeXt Write</div>
+      <div>
+        <Link href='/'>NeXt Write</Link></div>
       <div className="flex gap-4">
         {links.map((link) => (
           <Link className={`${path === link.href ? "text-slate-500 font-bold" : "text-zinc-200"}`} key={link.name} href={link.href}>
