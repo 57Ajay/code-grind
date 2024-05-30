71fbfd3bb1b2b40d89d02f43d55b77a190febbde
57Ajay <57ajay.u@gmail.com>
2024-05-30 18:46:10 +0530

    fixing footer bug in small devices

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,21 @@
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { redirect } from "next/navigation";
 
-const Home = async()=>{
+const Home = async () => {
   const { isAuthenticated } = getKindeServerSession();
-  if (!(await isAuthenticated())){
+  if (!(await isAuthenticated())) {
     redirect("/api/auth/login");
   }
-  return(
-    <main className="flex max-h-screen flex-col pt-24 text-center py-2">
-      <h1 className="text-5xl font-bold mb-7 text-stone-900"> Welcome to the NextWrite</h1>
-      <p className="text-lg font-semibold mb-7 text-black">NextWrite is a haven for writers of all kinds, offering a platform to unleash your creativity and share your stories with the world. Whether you`r a seasoned author or just starting your writing journey, NextWrite provides the tools and resources you need to bring your ideas to life.
-
-      Here, you can explore a diverse range of writing formats, from captivating stories and thought-provoking poems to informative articles and engaging blog posts. NextWrite empowers you to express yourself freely and connect with a global community of passionate writers.
-
-      Join us on this exciting adventure of words and imagination. Share your unique voice, discover new perspectives, and let your creativity soar. NextWrite is your gateway to a world of endless possibilities, where your writing journey awaits.
+  return (
+    <main className="flex flex-col justify-center items-center pt-24 text-center py-2">
+      <h1 className="text-5xl font-bold mb-7 text-stone-900">Welcome to the NextWrite</h1>
+      <p className="text-lg font-semibold mb-7 text-black">
+        NextWrite is a haven for writers of all kinds, offering a platform to unleash your creativity and share your stories with the world. Whether you`re a seasoned author or just starting your writing journey, NextWrite provides the tools and resources you need to bring your ideas to life.
+        Here, you can explore a diverse range of writing formats, from captivating stories and thought-provoking poems to informative articles and engaging blog posts. NextWrite empowers you to express yourself freely and connect with a global community of passionate writers.
+        Join us on this exciting adventure of words and imagination. Share your unique voice, discover new perspectives, and let your creativity soar. NextWrite is your gateway to a world of endless possibilities, where your writing journey awaits.
       </p>
     </main>
-  )
+  );
 };
 
 export default Home;
--- src/components/container.tsx
+++ src/components/container.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Container = ( { children }: {children: React.ReactNode})=>{
     return(
-        <div className="max-w-[1280px] mx-auto px-4 min-h-[88vh] shadow-lg bg-slate-600 text-zinc-300 rounded-md border border-zinc-800"> 
+        <div className="flex-grow max-w-[1280px] mx-auto px-4 min-h-[88vh] shadow-lg bg-slate-600 text-zinc-300 rounded-md border border-zinc-800"> 
             {children}
         </div>
     )
